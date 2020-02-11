a7c626a796cb51372266027588699ca97fdb2a99
Yoram Shamir <yoram-s@qualisystems.com>
2020-02-11 16:36:53 +0200

    refactor for tgs

--- cloudshell/traffic/healthcheck.py
+++ cloudshell/traffic/healthcheck.py
@@ -1,13 +1,8 @@
 
-import logging
-import time
-
-from cloudshell.workflow.orchestration.sandbox import Sandbox
 from cloudshell.api.cloudshell_api import CloudShellAPISession
-from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
-from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
 
-from tg_helper import get_qs_logger
+from common import (TrafficDriver, TrafficHandler, get_resources_from_reservation, get_reservation_id,
+                    get_connection_details_from_resource)
 
 ACS_MODEL = 'Acs'
 ACS_STATUS_MODEL = 'Acs_Status'
@@ -24,114 +19,6 @@ JIRA_MODEL = 'Jira'
 CABLE_MODEM_MODEL = 'Cable_Modem'
 
 
-class WriteMessageToReservationOutputHandler(logging.Handler):
-
-    def __init__(self, sandbox):
-        self.sandbox = sandbox
-        if type(self.sandbox) == Sandbox:
-            self.session = self.sandbox.automation_api
-            self.sandbox_id = self.sandbox.id
-        else:
-            self.session = CloudShellAPISession(host=sandbox.connectivity.server_address,
-                                                token_id=sandbox.connectivity.admin_auth_token,
-                                                domain=sandbox.reservation.domain)
-            self.sandbox_id = get_reservation_id(sandbox)
-        super(self.__class__, self).__init__()
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.session.WriteMessageToReservationOutput(self.sandbox_id, log_entry)
-
-
-def set_live_status(context, report):
-    cs_session = CloudShellAPISession(host=context.connectivity.server_address,
-                                      token_id=context.connectivity.admin_auth_token,
-                                      domain=context.reservation.domain)
-    if report['result']:
-        liveStatusName = 'Online'
-    else:
-        liveStatusName = 'Error'
-    cs_session.SetServiceLiveStatus(reservationId=get_reservation_id(context),
-                                    serviceAlias=context.resource.name,
-                                    liveStatusName=liveStatusName,
-                                    additionalInfo='tool_tip')
-
-
-def get_reservation_id(context):
-    """
-    :param ResourceCommandContext context:
-    """
-    try:
-        return context.reservation.reservation_id
-    except Exception as _:
-        return context.reservation.id
-
-
-def add_resources_to_reservation(context, *resources_full_path):
-    """
-    :param ResourceCommandContext context:
-    """
-    reservation_id = get_reservation_id(context)
-    cs_session = CloudShellAPISession(host=context.connectivity.server_address,
-                                      token_id=context.connectivity.admin_auth_token,
-                                      domain=context.reservation.domain)
-    cs_session.AddResourcesToReservation(reservationId=reservation_id, resourcesFullPath=list(resources_full_path),
-                                         shared=True)
-    all_resources = cs_session.GetReservationDetails(reservation_id).ReservationDescription.Resources
-    new_resources = [r for r in all_resources if r.Name in resources_full_path]
-    while len(new_resources) != len(resources_full_path):
-        time.sleep(1)
-        all_resources = cs_session.GetReservationDetails(reservation_id).ReservationDescription.Resources
-        new_resources = [r for r in all_resources if r.Name in resources_full_path]
-    return new_resources
-
-
-def add_service_to_reservation(context, service_name, alias=None, attributes=[]):
-    """
-    :param ResourceCommandContext context:
-    """
-    if not alias:
-        alias = service_name
-    reservation_id = get_reservation_id(context)
-    cs_session = CloudShellAPISession(host=context.connectivity.server_address,
-                                      token_id=context.connectivity.admin_auth_token,
-                                      domain=context.reservation.domain)
-    cs_session.AddServiceToReservation(reservationId=reservation_id,
-                                       serviceName=service_name, alias=alias,
-                                       attributes=attributes)
-    all_services = cs_session.GetReservationDetails(reservation_id).ReservationDescription.Services
-    new_service = [s for s in all_services if s.ServiceName == service_name]
-    while not new_service:
-        time.sleep(1)
-        all_services = cs_session.GetReservationDetails(reservation_id).ReservationDescription.Services
-        new_service = [s for s in all_services if s.ServiceName == service_name]
-    return new_service[0]
-
-
-def get_resources_from_reservation(context, resource_model):
-    """
-    :param ResourceCommandContext context: resource command context
-    """
-    reservation_id = get_reservation_id(context)
-    cs_session = CloudShellAPISession(host=context.connectivity.server_address,
-                                      token_id=context.connectivity.admin_auth_token,
-                                      domain=context.reservation.domain)
-    resources = cs_session.GetReservationDetails(reservation_id).ReservationDescription.Resources
-    return [r for r in resources if r.ResourceModelName == resource_model]
-
-
-def get_services_from_reservation(context, service_name):
-    """
-    :param ResourceCommandContext context: resource command context
-    """
-    reservation_id = get_reservation_id(context)
-    cs_session = CloudShellAPISession(host=context.connectivity.server_address,
-                                      token_id=context.connectivity.admin_auth_token,
-                                      domain=context.reservation.domain)
-    services = cs_session.GetReservationDetails(reservation_id).ReservationDescription.Services
-    return [s for s in services if s.ServiceName == service_name]
-
-
 def get_mac_from_cable_modem(context):
     """
     :param ResourceCommandContext context: resource command context
@@ -142,23 +29,6 @@ def get_mac_from_cable_modem(context):
                                       domain=context.reservation.domain)
     cm_resource_details = cs_session.GetResourceDetails(cm_resource.Name)
     return [a.Value for a in cm_resource_details.ResourceAttributes if a.Name == 'Cable_Modem.mac_address'][0]
-
-
-def get_connection_details_from_resource(context, resource_model, requested_details=['User', 'Password']):
-    """
-    :param ResourceCommandContext context: resource command context
-    """
-    cm_resource = get_resources_from_reservation(context, resource_model)[0]
-    cs_session = CloudShellAPISession(host=context.connectivity.server_address,
-                                      token_id=context.connectivity.admin_auth_token,
-                                      domain=context.reservation.domain)
-    resource_details = cs_session.GetResourceDetails(cm_resource.Name)
-    details = {}
-    details['Address'] = resource_details.Address
-    for requested_detail in requested_details:
-        attribute_name = '{}.{}'.format(resource_model, requested_detail)
-        details[requested_detail] = [a.Value for a in resource_details.ResourceAttributes if a.Name == attribute_name][0]
-    return details
 
 
 def get_connection_details_from_cnr(context):
@@ -188,18 +58,10 @@ def get_mac_from_cable_modem(context):
     return [a.Value for a in cm_resource_details.ResourceAttributes if a.Name == 'Cable_Modem.mac_address'][0]
 
 
-class HealthCheckDriver(ResourceDriverInterface):
+class HealthCheckDriver(TrafficDriver):
 
-    def initialize(self, context):
-        self.logger = get_qs_logger(log_group='traffic_shells', log_file_prefix=context.resource.name)
-        self.logger.setLevel(logging.DEBUG)
-        self.handler.initialize(context, self.logger)
-
-    def cleanup(self):
-        pass
-
-    def get_inventory(self, context):
-        return self.handler.load_inventory(context)
+    def initialize(self, context, log_group='healthcheck_shells'):
+        super(HealthCheckDriver, self).initialize(context, log_group)
 
     def health_check(self, context, mac_address):
         if not mac_address:
@@ -207,36 +69,12 @@ class HealthCheckDriver(ResourceDriverInterface):
         return self.handler.health_check(context, mac_address)
 
 
-class TrafficHandler(object):
-
-    def initialize(self, resource, logger, packages_loggers=[]):
-
-        self.resource = resource
-        self.service = resource
-        self.logger = logger
-
-        for package_logger in packages_loggers:
-            package_logger = logging.getLogger(package_logger)
-            package_logger.setLevel(self.logger.level)
-            for handler in self.logger.handlers:
-                if handler not in package_logger.handlers:
-                    package_logger.addHandler(handler)
-
-
 class HealthCheckHandler(TrafficHandler):
 
     def initialize(self, context, logger, resource):
-
-        super(HealthCheckHandler, self).initialize(resource=resource, logger=logger, packages_loggers=['pycmts', 'pylgi'])
-
-        self.address = context.resource.address
-        if not self.address:
-            self.address = self.resource.address
-        self.user = self.resource.user
-        self.logger.debug('User - {}'.format(self.user))
-        self.logger.debug('Encripted password - {}'.format(self.resource.password))
-        self.password = CloudShellSessionContext(context).get_api().DecryptPassword(self.resource.password).Value
-        self.logger.debug('Password - {}'.format(self.password))
+        super(HealthCheckHandler, self).initialize(resource=resource, logger=logger,
+                                                   packages_loggers=['pycmts', 'pylgi'])
+        self.get_connection_details(context)
 
     def health_check(self):
         report = {}
--- cloudshell/traffic/tg_helper.py
+++ cloudshell/traffic/tg_helper.py
@@ -1,13 +1,9 @@
 
-import time
 import re
 import logging
 
-from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
-from cloudshell.shell.core.context_utils import get_resource_name
 from cloudshell.core.logger.qs_logger import get_qs_logger
-
-import quali_rest_api_helper
+from cloudshell.api.cloudshell_api import CloudShellAPISession
 
 
 def get_logger(context):
@@ -53,20 +49,18 @@ def get_reservation_resources(session, reservation_id, *models):
     return models_resources
 
 
-def get_family_attribute(session, resource, attribute):
-    return session.GetAttributeValue(resource.Name, _family_attribute_name(resource, attribute))
+def get_family_attribute(context, resource, attribute):
+    cs_session = CloudShellAPISession(host=context.connectivity.server_address,
+                                      token_id=context.connectivity.admin_auth_token,
+                                      domain=context.reservation.domain)
+    return cs_session.GetAttributeValue(resource.Name, _family_attribute_name(resource, attribute))
 
 
-def set_family_attribute(session, resource, attribute, value):
-    session.SetAttributeValue(resource.Name, _family_attribute_name(resource, attribute), value)
-
-
-def enqueue_keep_alive(context):
-    my_api = CloudShellSessionContext(context).get_api()
-    reservation_id = context.reservation.reservation_id
-    resource_name = get_resource_name(context=context)
-    my_api.EnqueueCommand(reservationId=reservation_id, targetName=resource_name, commandName="keep_alive",
-                          targetType="Service")
+def set_family_attribute(context, resource, attribute, value):
+    cs_session = CloudShellAPISession(host=context.connectivity.server_address,
+                                      token_id=context.connectivity.admin_auth_token,
+                                      domain=context.reservation.domain)
+    cs_session.SetAttributeValue(resource.Name, _family_attribute_name(resource, attribute), value)
 
 
 def get_address(port_resource):
@@ -75,20 +69,6 @@ def get_address(port_resource):
 
 def is_blocking(blocking):
     return True if blocking.lower() == "true" else False
-
-
-def write_to_reservation_out(context, message):
-    my_api = CloudShellSessionContext(context).get_api()
-    my_api.WriteMessageToReservationOutput(context.reservation.reservation_id, message)
-
-
-def attach_stats_csv(context, logger, view_name, output, suffix='csv'):
-    quali_api_helper = quali_rest_api_helper.create_quali_api_instance(context, logger)
-    quali_api_helper.login()
-    full_file_name = view_name.replace(' ', '_') + '_' + time.ctime().replace(' ', '_') + '.' + suffix
-    quali_api_helper.upload_file(context.reservation.reservation_id, file_name=full_file_name, file_stream=output)
-    write_to_reservation_out(context, 'Statistics view saved in attached file - ' + full_file_name)
-    return full_file_name
 
 
 def _family_attribute_name(resource, attribute):
